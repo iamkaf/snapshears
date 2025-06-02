91f7658d5dcca7e4c89754133f28013048d8cb93
Kaf <coffeechopdev@gmail.com>
2025-06-02 19:01:15 -0300

    wip: rename a bunch of stuff to snapshears and get ready for publish

---
+++ forge/src/main/java/com/iamkaf/snapshears/platform/ForgePlatformHelper.java
@@ -0,0 +1,26 @@
+package com.iamkaf.snapshears.platform;
+
+import com.iamkaf.snapshears.platform.services.IPlatformHelper;
+import net.minecraftforge.fml.ModList;
+import net.minecraftforge.fml.loading.FMLLoader;
+
+public class ForgePlatformHelper implements IPlatformHelper {
+
+    @Override
+    public String getPlatformName() {
+
+        return "Forge";
+    }
+
+    @Override
+    public boolean isModLoaded(String modId) {
+
+        return ModList.get().isLoaded(modId);
+    }
+
+    @Override
+    public boolean isDevelopmentEnvironment() {
+
+        return !FMLLoader.isProduction();
+    }
+}
---
+++ neoforge/src/main/java/com/iamkaf/snapshears/platform/NeoForgePlatformHelper.java
@@ -0,0 +1,26 @@
+package com.iamkaf.snapshears.platform;
+
+import com.iamkaf.snapshears.platform.services.IPlatformHelper;
+import net.neoforged.fml.ModList;
+import net.neoforged.fml.loading.FMLLoader;
+
+public class NeoForgePlatformHelper implements IPlatformHelper {
+
+    @Override
+    public String getPlatformName() {
+
+        return "NeoForge";
+    }
+
+    @Override
+    public boolean isModLoaded(String modId) {
+
+        return ModList.get().isLoaded(modId);
+    }
+
+    @Override
+    public boolean isDevelopmentEnvironment() {
+
+        return !FMLLoader.isProduction();
+    }
+}
